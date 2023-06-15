b295e2ddab22e051f8136e4d7be3e19726ab38da
PARKPARKWOO <wy9295@naver.com>
2023-06-15 16:20:16 +0900

    RDS 변경

--- src/main/java/com/example/baekersolved/domain/api/feign/StudyRuleFeign.java
+++ src/main/java/com/example/baekersolved/domain/api/feign/StudyRuleFeign.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-@FeignClient(name = "StudyRule", url = "StudyRuleURL 작성하기")
+@FeignClient(name = "StudyRule", url = "http://${custom.server}:9000")
 public interface StudyRuleFeign {
     @RequestMapping(method = RequestMethod.GET, value = "/api/studyrule/v1/search")
     RsData<List<StudyRuleConsumeDto>> getStudyRule();
