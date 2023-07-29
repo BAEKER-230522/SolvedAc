0f9e0aea9db1b2e3a26440c5d58c657dc655f005
PARKPARKWOO <wy9295@naver.com>
2023-07-29 15:54:40 +0900

    agent application 으로 변경

--- src/test/java/com/example/baekersolved/api/ApiManagerTest.java
+++ src/test/java/com/example/baekersolved/api/ApiManagerTest.java
@@ -1,7 +1,11 @@
 package com.example.baekersolved.api;
 
 import com.example.baekersolved.domain.SolvedApiManager;
-import feign.Response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,8 +18,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.print.attribute.standard.Media;
-
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.http.HttpClient;
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -28,6 +33,7 @@ public class ApiManagerTest {
     RestTemplate restTemplate;
     @Autowired
     SolvedApiManager manager;
+
     @Test
     @DisplayName("사용자 정보 가져오기")
     void getUserInformation() {
@@ -37,16 +43,16 @@ public class ApiManagerTest {
             headers.setContentType(APPLICATION_JSON);
             headers.setAccept(Collections.singletonList(APPLICATION_JSON));
             headers.add(HttpHeaders.ACCEPT, "application/json");
-            headers.add(HttpHeaders.USER_AGENT, "Mozilla/5.0");
+            headers.add(HttpHeaders.USER_AGENT, "Application");
             HttpEntity<String> entity = new HttpEntity<>(headers);
-             response = restTemplate
-                    .exchange(manager.getUserInformation("wy9295"), HttpMethod.GET, entity,String.class);
-        }catch (Exception e) {
+            response = restTemplate
+                    .exchange(manager.getUserInformation("wy9295"), HttpMethod.GET, entity, String.class);
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertEquals(APPLICATION_JSON_VALUE, response.getHeaders().getContentType().getType() + "/" +
-                                                    response.getHeaders().getContentType().getSubtype());
+                response.getHeaders().getContentType().getSubtype());
     }
 }
