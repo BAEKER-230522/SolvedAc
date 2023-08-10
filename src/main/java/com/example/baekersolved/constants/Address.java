576b53b74a361a5d8dbf3c30685eaa16bceff649
PARKPARKWOO <wy9295@naver.com>
2023-08-11 01:22:09 +0900

    Feat: studyrule 조회 하여 미션 성공 여부 체크

--- src/main/java/com/example/baekersolved/constants/Address.java
+++ src/main/java/com/example/baekersolved/constants/Address.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 
 public class Address {
     public static final String MEMBER_ALL = "/api/member/get/v1/all";
-    public static final String MEMBER_LASTSOLVEDID_UPDATE = "/api/member/v1/last-solved";
+    public static final String MEMBER_LASTSOLVEDID_UPDATE = "/api/member/v1/last-solved"; // kafka
     public static final String MEMBER_SOLVED_UPDATE = "/api/member/solved";
     public static final String STUDYRULE_ALL = "/api/studyrule/v1/search";
+    public static final String STUDYRULE_UPDATE = "/api/studyrule/v1/studyrules/"; // kafka
+    public static final String STUDYRULE_UPDATE_END = "/solved"; // kakfa
     public static final String SOLVED_BASE_URL = "https://solved.ac";
     public static final String SOLVED_PROFILE = "/profile/"; // /+id 입력
     public static final String SOLVED_PROBLEM_URL = "/problems/level/"; // /+level 입력 ex) 0~30
