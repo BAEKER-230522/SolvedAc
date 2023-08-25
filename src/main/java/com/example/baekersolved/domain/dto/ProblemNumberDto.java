fd9fb65bb52ee0670766a37b541b50e5a244d629
PARKPARKWOO <wy9295@naver.com>
2023-08-25 21:59:04 +0900

    Feat: 시간 , 메모리 추가

--- src/main/java/com/example/baekersolved/domain/dto/ProblemNumberDto.java
+++ src/main/java/com/example/baekersolved/domain/dto/ProblemNumberDto.java
@@ -1,4 +1,4 @@
 package com.example.baekersolved.domain.dto;
 
-public record ProblemNumberDto(String problemNumber) {
+public record ProblemNumberDto(String problemNumber, String time, String memory) {
 }
