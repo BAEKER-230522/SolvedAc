856b2908fe264d6615ebb4a28573e26f0ee06cff
PARKPARKWOO <wy9295@naver.com>
2023-08-26 19:08:16 +0900

    Delete: rule 연관관계 삭제

--- src/main/java/com/example/baekersolved/domain/dto/request/StudyRuleConsumeDto.java
+++ src/main/java/com/example/baekersolved/domain/dto/request/StudyRuleConsumeDto.java
@@ -1,12 +1,9 @@
 package com.example.baekersolved.domain.dto.request;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-
 public record StudyRuleConsumeDto(
 
         Long id, //
         String name,
-        String about,
-        Long ruleId
+        String about
 ) {
 }
