84ac9c0e14a4242a91201d12a2ded0bc432226fb
PARKPARKWOO <wy9295@naver.com>
2023-09-17 15:40:52 +0900

    fix 드라이버 경로 수정

--- src/main/java/com/example/baekersolved/domain/model/SolvedCrawling.java
+++ src/main/java/com/example/baekersolved/domain/model/SolvedCrawling.java
@@ -225,7 +225,7 @@ public class SolvedCrawling {
             process.waitFor();
             System.setProperty("webdriver.chrome.driver", "drivers/chromedriver_mac");
         } else if (os.contains("linux")) {
-            System.setProperty("webdriver.chrome.driver", "/usr/bin/chrome-linux64/chromedriver");
+            System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver-linux64/chromedriver");
         }
 
         ChromeOptions chromeOptions = new ChromeOptions();
